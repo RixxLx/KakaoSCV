40230e207dc8736a5856d60d98bf129ab383323d
Shane Lx <39976362+RixxLx@users.noreply.github.com>
2023-03-20 18:20:14 +0900

    Use tabs. Configure .prettierrc properly

--- index.js
+++ index.js
@@ -7,72 +7,70 @@ console.log(process.env.OPENAI_API);
 const server = new Server({ useKakaoLink: false });
 
 const options = {
-  method: "POST",
-  hostname: "api.openai.com",
-  path: "/v1/chat/completions",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: process.env.OPENAI_API,
-  },
-  maxRedirects: 20,
+	method: "POST",
+	hostname: "api.openai.com",
+	path: "/v1/chat/completions",
+	headers: {
+		"Content-Type": "application/json",
+		Authorization: process.env.OPENAI_API,
+	},
+	maxRedirects: 20,
 };
 
 server.on("message", async (msg) => {
-  console.log("[" + msg.room + "] " + msg.sender.name + " : " + msg.content);
+	console.log("[" + msg.room + "] " + msg.sender.name + " : " + msg.content);
 
-  const prefix = ">";
-  if (!msg.content.startsWith(prefix)) return;
+	const prefix = ">";
+	if (!msg.content.startsWith(prefix)) return;
 
-  const args = msg.content.split(" ");
-  const cmd = args.shift()?.slice(prefix.length);
+	const args = msg.content.split(" ");
+	const cmd = args.shift()?.slice(prefix.length);
 
-  switch (cmd) {
-    case "ping": // not accurate
-      const timestamp = Date.now();
-      msg.replyText(`${Date.now() - timestamp}ms, pong!`);
-      break;
-    case ">":
-      const allMsg = msg.content.slice(2);
-      var req = https.request(options, function (res) {
-        var chunks = [];
+	switch (cmd) {
+		case "ping": // not accurate
+			const timestamp = Date.now();
+			msg.replyText(`${Date.now() - timestamp}ms, pong!`);
+			break;
+		case ">":
+			const allMsg = msg.content.slice(2);
+			var req = https.request(options, function (res) {
+				var chunks = [];
 
-        res.on("data", function (chunk) {
-          chunks.push(chunk);
-        });
+				res.on("data", function (chunk) {
+					chunks.push(chunk);
+				});
 
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          let resExtract = JSON.parse(body.toString());
-          //   For debugging
-          //   console.log(resExtract);
-          console.log(
-            "ChatGPT : " + resExtract.choices[0].message.content.trim()
-          );
-          msg.replyText(resExtract.choices[0].message.content.trim());
-        });
+				res.on("end", function (chunk) {
+					var body = Buffer.concat(chunks);
+					let resExtract = JSON.parse(body.toString());
+					//   For debugging
+					//   console.log(resExtract);
+					console.log("ChatGPT : " + resExtract.choices[0].message.content.trim());
+					msg.replyText(resExtract.choices[0].message.content.trim());
+				});
 
-        res.on("error", function (error) {
-          console.error(error);
-        });
-      });
+				res.on("error", function (error) {
+					console.error(error);
+				});
+			});
 
-      var postData = JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: allMsg,
-          },
-        ],
-        temperature: 0.7,
-      });
+			var postData = JSON.stringify({
+				model: "gpt-3.5-turbo",
+				messages: [
+					{
+						role: "user",
+						content: allMsg,
+					},
+				],
+				temperature: 0.7,
+			});
 
-      req.write(postData);
+			req.write(postData);
 
-      req.end();
+			req.end();
 
-      break;
-  }
+			break;
+	}
 });
 
 server.start(3000);
